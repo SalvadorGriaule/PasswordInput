f01480c6fd405b361379001990d4f8bf72aa9ad6
SalvadorGriaule <morales.quentin@gmx.fr>
2025-11-16 17:42:12 +0100

    first commit

---
+++ src/lib/PassWordInput.svelte
@@ -0,0 +1,96 @@
+<script lang="ts">
+    import { animate } from "animejs";
+    import CheckSvg from "./CheckSvg.svelte";
+    import { inputMinL, checkingAll, testList } from "./inputCheck.ts";
+
+    interface PassCheck {
+        fn: Function;
+        msg: string;
+    }
+
+    let {
+        changeMode = false,
+        boolPW = $bindable(false),
+        greatPW = $bindable(""),
+        Password = $bindable(""),
+    }: {
+        changeMode?: boolean;
+        boolPW?: boolean;
+        greatPW?: string;
+        Password?: string;
+    } = $props();
+
+    let divCheck: HTMLDivElement;
+    let allCheck: PassCheck[] = $state([
+        { fn: inputMinL, msg: "Plus de 8 caractére" },
+        { fn: testList.numCheck, msg: "Au moins un nombre" },
+        { fn: testList.oneUpper, msg: "Au moins une Majuscule" },
+        { fn: testList.oneLower, msg: "Au moins une Minuscule" },
+        {
+            fn: testList.specChar,
+            msg: "Au moins un caractere special <br /> (#|~!?,.§€$£%:*())",
+        },
+    ]);
+
+    let passwordInput: string = $state("");
+    let outCheck: boolean[] = $state(Array(allCheck.length).fill(false));
+    let checking: string = $derived(checkingAll([...outCheck], passwordInput));
+    let isFocus = $state(false);
+
+    let widthCheckDiv = $state(0);
+    let heightCheckDiv: 150 | 82 = $derived(widthCheckDiv < 470 ? 150 : 82);
+
+    $inspect(widthCheckDiv);
+
+    $effect(() => {
+        let heightAni = isFocus ? `${heightCheckDiv}px` : "0px";
+        animate(divCheck, {
+            height: heightAni,
+            duration: 250,
+            easing: "easeInQuad",
+        });
+    });
+
+    $effect(() => {
+        greatPW = checking;
+        boolPW = checking == "border-green-600";
+        Password = passwordInput;
+    });
+</script>
+
+<div class="w-full">
+    <input
+        bind:focused={isFocus}
+        bind:value={passwordInput}
+        type="password"
+        name="Password"
+        class="{checking} border-2 p-1 placeholder:text-center w-full"
+        placeholder={!changeMode ? "Mot de passe" : "Nouveau mot de passe"}
+        required
+    />
+
+    <div
+        bind:this={divCheck}
+        bind:clientWidth={widthCheckDiv}
+        class="{widthCheckDiv < 470 && widthCheckDiv > 300
+            ? 'justify-center'
+            : 'justify-start'} h-0 flex overflow-hidden"
+    >
+        <div
+            class="flex flex-col flex-wrap ml-2 my-1"
+            style:height={heightCheckDiv + "px"}
+        >
+            {#each allCheck as check, i}
+                <div class="flex space-x-2">
+                    <div class="w-4">
+                        <CheckSvg
+                            check={check.fn(passwordInput)}
+                            bind:out={outCheck[i]}
+                        />
+                    </div>
+                    <p>{@html check.msg}</p>
+                </div>
+            {/each}
+        </div>
+    </div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import PassWordInput from "$lib/PassWordInput.svelte";
+  let boolPW = $state(false);
+  let disabled = $derived(!boolPW)
+  
+</script>
+
+
+<div class="flex flex-col items-center w-full h-screen justify-center">
+  <div class="bg-white p-2 rounded-2xl drop-shadow-2xl w-full lg:w-3/4">
+    <PassWordInput bind:boolPW />
+    <div class="w-full flex justify-center mt-2">
+      <button {disabled} class="p-2 rounded-2xl bg-green-600 text-white disabled:bg-gray-600">Envoyer</button>
+    </div>
+  </div>
+</div>
